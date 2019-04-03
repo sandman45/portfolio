c53583ae70179c6c3aa42f36462e5477e1a6b44d
EC2 Default User <ec2-user@ip-172-31-24-228.us-west-2.compute.internal>
2019-04-03 21:11:44 +0000

    server stuff

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('test'));
+
+app.get('/', function(req,res) {
+  const indexPath = path.join(__dirname, '../');
+  console.log(`$indexPath}`);
+  res.sendFile('index.html',{root: indexPath});
+});
+
+
+app.listen(port, (err) => {
+    if (err) {
+        console.log(`error - ${err}`);
+    }
+    console.log(`server running on port: ${port}`);
+});
